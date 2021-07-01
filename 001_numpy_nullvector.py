3683a6976002170b1604f3e691a33cfab1ddf533
sadpasmgp <86211117+sadpasmgp@users.noreply.github.com>
2021-07-01 11:30:53 +0530

    Numpy null vector

---
+++ 001_numpy_nullvector.py
@@ -0,0 +1,14 @@
+"""
+Create a null vector of size 10 
+"""
+
+
+import numpy as np
+nullvec = np.zeros(10)
+print(nullvec)
+
+
+"""
+PS C:\Users\SP\Desktop\DiveintoData\Numpy> python .\001_numpy_nullvector.py                   [0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+PS C:\Users\SP\Desktop\DiveintoData\Numpy>  
+"""
